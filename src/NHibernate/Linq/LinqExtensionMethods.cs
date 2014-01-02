a980a6a3a03fe9aa94d5255f395ce66ecad018c4
Onur Gumus <onurgumus@hotmail.com>
2014-01-02 20:23:09 +0200

    work on async

--- src/NHibernate/Linq/LinqExtensionMethods.cs
+++ src/NHibernate/Linq/LinqExtensionMethods.cs
@@ -6,6 +6,7 @@ using NHibernate.Impl;
 using Remotion.Linq;
 using Remotion.Linq.Parsing.ExpressionTreeVisitors;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace NHibernate.Linq
 {
@@ -19,6 +20,176 @@ namespace NHibernate.Linq
 		public static IQueryable<T> Query<T>(this IStatelessSession session)
 		{
 			return new NhQueryable<T>(session.GetSessionImplementation());
+		}
+
+	    /// <summary>Determines whether a sequence contains any elements.</summary>
+        /// <returns>true if the source sequence contains any elements; otherwise, false.</returns>
+        /// <param name="source">A sequence to check for being empty.</param>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///   <paramref name="source" /> is null.</exception>
+        public static async Task<bool> AnyAsync<TSource>(this IQueryable<TSource> source)
+        {
+            if (source == null)
+            {
+              throw new ArgumentNullException("source");
+            }
+			INhQueryProvider provider = (INhQueryProvider)source.Provider;
+
+			var typeArray = new System.Type[] { typeof(TSource) };
+            MethodInfo methodInfo = ((MethodInfo)MethodBase.GetCurrentMethod()).MakeGenericMethod(typeArray);
+            Expression[] expression = new Expression[] { source.Expression };
+            return await provider.ExecuteAsync<bool>(Expression.Call(null, methodInfo, expression));
+        }
+
+        /// <summary>Determines whether any element of a sequence satisfies a condition.</summary>
+        /// <returns>true if any elements in the source sequence pass the test in the specified predicate; otherwise, false.</returns>
+        /// <param name="source">A sequence whose elements to test for a condition.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///  <paramref name="source" /> or <paramref name="predicate" /> is null.</exception>
+        public static async Task<bool> AnyAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source");
+			}
+			if (predicate == null)
+			{
+				throw new ArgumentNullException("predicate");
+			}
+			INhQueryProvider provider = (INhQueryProvider)source.Provider;
+			MethodInfo methodInfo = ((MethodInfo)MethodBase.GetCurrentMethod()).MakeGenericMethod(new System.Type[] { typeof(TSource) });
+			Expression[] expression = new Expression[] { source.Expression, Expression.Quote(predicate) };
+			return await provider.ExecuteAsync<bool>(Expression.Call(null, methodInfo, expression));
+		}
+		 /// <summary>Returns the number of elements in a sequence.</summary>
+        /// <returns>The number of elements in the input sequence.</returns>
+        /// <param name="source">The <see cref="T:System.Linq.IQueryable`1" /> that contains the elements to be counted.</param>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///   <paramref name="source" /> is null.</exception>
+        /// <exception cref="T:System.OverflowException">The number of elements in <paramref name="source" /> is larger than <see cref="F:System.Int32.MaxValue" />.</exception>
+        public static async Task<int> CountAsync<TSource>(this IQueryable<TSource> source)
+        {
+            if (source == null)
+            {
+               throw new ArgumentNullException("source");
+            }
+            INhQueryProvider provider = (INhQueryProvider)source.Provider;
+            MethodInfo methodInfo = ((MethodInfo)MethodBase.GetCurrentMethod()).MakeGenericMethod(new System.Type[] { typeof(TSource) });
+            Expression[] expression = new Expression[] { source.Expression };
+            return await provider.ExecuteAsync<int>(Expression.Call(null, methodInfo, expression));
+        }
+
+        /// <summary>Returns the number of elements in the specified sequence that satisfies a condition.</summary>
+        /// <returns>The number of elements in the sequence that satisfies the condition in the predicate function.</returns>
+        /// <param name="source">An <see cref="T:System.Linq.IQueryable`1" /> that contains the elements to be counted.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///   <paramref name="source" /> or <paramref name="predicate" /> is null.</exception>
+        /// <exception cref="T:System.OverflowException">The number of elements in <paramref name="source" /> is larger than <see cref="F:System.Int32.MaxValue" />.</exception>
+		public static async Task<int> CountAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source");
+			}
+			if (predicate == null)
+			{
+				throw new ArgumentNullException("predicate");
+			}
+			INhQueryProvider provider = (INhQueryProvider)source.Provider;
+			MethodInfo methodInfo = ((MethodInfo)MethodBase.GetCurrentMethod()).MakeGenericMethod(new System.Type[] { typeof(TSource) });
+			Expression[] expression = new Expression[] { source.Expression, Expression.Quote(predicate) };
+			return  await provider.ExecuteAsync<int>(Expression.Call(null, methodInfo, expression));
+		}
+
+
+		/// <summary>Returns the first element of a sequence.</summary>
+		/// <returns>The first element in <paramref name="source" />.</returns>
+		/// <param name="source">The <see cref="T:System.Linq.IQueryable`1" /> to return the first element of.</param>
+		/// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+		/// <exception cref="T:System.ArgumentNullException">
+		///   <paramref name="source" /> is null.</exception>
+		/// <exception cref="T:System.InvalidOperationException">The source sequence is empty.</exception>
+		public static async Task<TSource> FirstAsync<TSource>(this IQueryable<TSource> source)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source");
+			}
+			INhQueryProvider provider = (INhQueryProvider)source.Provider;
+			MethodInfo methodInfo = ((MethodInfo)MethodBase.GetCurrentMethod()).MakeGenericMethod(new System.Type[] { typeof(TSource) });
+			Expression[] expression = new Expression[] { source.Expression };
+			return await provider.ExecuteAsync<TSource>(Expression.Call(null, methodInfo, expression));
+		}
+
+		/// <summary>Returns the first element of a sequence that satisfies a specified condition.</summary>
+		/// <returns>The first element in <paramref name="source" /> that passes the test in <paramref name="predicate" />.</returns>
+		/// <param name="source">An <see cref="T:System.Linq.IQueryable`1" /> to return an element from.</param>
+		/// <param name="predicate">A function to test each element for a condition.</param>
+		/// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+		/// <exception cref="T:System.ArgumentNullException">
+		///   <paramref name="source" /> or <paramref name="predicate" /> is null.</exception>
+		/// <exception cref="T:System.InvalidOperationException">No element satisfies the condition in <paramref name="predicate" />.-or-The source sequence is empty.</exception>
+		public static  async Task<TSource> FirstAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source");
+			}
+			if (predicate == null)
+			{
+				throw new ArgumentNullException("predicate");
+			}
+			INhQueryProvider provider = (INhQueryProvider)source.Provider;
+			MethodInfo methodInfo = ((MethodInfo)MethodBase.GetCurrentMethod()).MakeGenericMethod(new System.Type[] { typeof(TSource) });
+			Expression[] expression = new Expression[] { source.Expression, Expression.Quote(predicate) };
+			return await provider.ExecuteAsync<TSource>(Expression.Call(null, methodInfo, expression));
+		}
+
+		/// <summary>Returns the first element of a sequence, or a default value if the sequence contains no elements.</summary>
+		/// <returns>default(<paramref name="TSource" />) if <paramref name="source" /> is empty; otherwise, the first element in <paramref name="source" />.</returns>
+		/// <param name="source">The <see cref="T:System.Linq.IQueryable`1" /> to return the first element of.</param>
+		/// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+		/// <exception cref="T:System.ArgumentNullException">
+		///   <paramref name="source" /> is null.</exception>
+		public static async Task<TSource> FirstOrDefaultAsync<TSource>(this IQueryable<TSource> source)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source");
+			}
+			INhQueryProvider provider = (INhQueryProvider)source.Provider;
+			MethodInfo methodInfo = ((MethodInfo)MethodBase.GetCurrentMethod()).MakeGenericMethod(new System.Type[] { typeof(TSource) });
+			Expression[] expression = new Expression[] { source.Expression };
+			return await provider.ExecuteAsync<TSource>(Expression.Call(null, methodInfo, expression));
+		}
+
+		/// <summary>Returns the first element of a sequence that satisfies a specified condition or a default value if no such element is found.</summary>
+		/// <returns>default(<paramref name="TSource" />) if <paramref name="source" /> is empty or if no element passes the test specified by <paramref name="predicate" />; otherwise, the first element in <paramref name="source" /> that passes the test specified by <paramref name="predicate" />.</returns>
+		/// <param name="source">An <see cref="T:System.Linq.IQueryable`1" /> to return an element from.</param>
+		/// <param name="predicate">A function to test each element for a condition.</param>
+		/// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+		/// <exception cref="T:System.ArgumentNullException">
+		///   <paramref name="source" /> or <paramref name="predicate" /> is null.</exception>
+		public static async Task<TSource> FirstOrDefaultAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source");
+			}
+			if (predicate == null)
+			{
+				throw new ArgumentNullException("predicate");
+			}
+			INhQueryProvider provider = (INhQueryProvider)source.Provider;
+			MethodInfo methodInfo = ((MethodInfo)MethodBase.GetCurrentMethod()).MakeGenericMethod(new System.Type[] { typeof(TSource) });
+			Expression[] expression = new Expression[] { source.Expression, Expression.Quote(predicate) };
+			return await provider.ExecuteAsync<TSource>(Expression.Call(null, methodInfo, expression));
 		}
 
 		public static IQueryable<T> Cacheable<T>(this IQueryable<T> query)
